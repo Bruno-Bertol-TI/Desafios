21219c96e9188212e37225c257a0185638e98b26
Bruno Bertol <bbertol2312@outlook.com>
2025-03-27 16:57:37 -0300

    Atualizando e atribuindo funcoes

--- calculadora-de-area/app.js
+++ calculadora-de-area/app.js
@@ -2,14 +2,61 @@
 //  e o perímetro de uma sala retangular, utilizando 
 // altura e largura que serão dadas como parâmetro.
 
+// Crie uma função que mostre na tela a área e o perímetro de uma sala circular, 
+// utilizando seu raio que será fornecido como parâmetro. Considere Pi = 3,14.
 
-function calc_area(altura, largura) {
-    area = altura * largura
-    perimetro = (altura*2) + (largura*2)
+while (loop == true) {
+    formas = prompt('Escolha a forma do local ou objeto que deseja calcular (circulo, triangulo, quadrado ou retangulo): ').toLowerCase();
+
+    if (formas == 'triangulo') {
+        entradaMedidas();
+        calcTriangulo();
+    } else {
+        if (formas == 'circulo') {
+            entradaMedidas();
+            calcCirculo();
+        }
+        if (formas == 'quadrado' || formas == 'retangulo') {
+            entradaMedidas();
+            calcQuadradoRetangulo();
+        }
+        alert('Voce digitou uma opcao inexistente, tente novamente');
+    }
 }
 
-alt = parseFloat(prompt('Digite a altura que deseja calcular: '))
-lar = parseFloat(prompt('Digite a largura que deseja calcular: '))
-calc_area(alt, lar)
-console.log(area, perimetro)
-alert(`A area é ${area}m² e o perimetro é ${perimetro}m de perimetro`)
+function entradaMedidas(formas) {
+    msgPrompt = 'Digite o valor da(o)';
+    if (formas == 'triangulo') {
+        base = prompt(`${msgPrompt} Base: `);
+        altura = prompt(`${msgPrompt} altura do ${formas}: `);
+        a = prompt(`${msgPrompt} lado A do ${formas}: `);
+        b = prompt(`${msgPrompt} lado B do ${formas}: `);
+    }
+    
+    if (formas == 'circulo') {
+        raio = prompt(`${msgPrompt} Raio: `);
+    }
+
+    if (formas == 'quadrado' || formas == 'retangulo') {
+        altura = prompt(`${msgPrompt} altura do ${formas}: `);
+        largura = prompt(`${msgPrompt} largura do ${formas}: `);
+    }
+
+    base, altura, a, b, raio, largura = parseFloat(base, altura, a, b, raio, largura);
+}
+
+function calcQuadradoRetangulo(altura, largura) {
+    area = altura * largura;
+    perimetro = (altura*2) + (largura*2);
+}
+
+function calcCirculo(raio) {
+    area = 3.14 * raio**2;
+    circunferencia = 2 * 3.14 * raio;
+}
+
+entradaMedidas(formas);
+calcCirculo(raio);
+calcQuadradoRetangulo(alt, dimensao);
+alternativaPerimetro = formas == 'circulo' ? circunferencia : perimetro;
+alert(`A area é ${area}m e o perimetro é ${alternativaPerimetro}m de perimetro`);
