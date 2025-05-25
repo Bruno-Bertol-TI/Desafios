b648f9e500854f264d42e5709b98fdf036e77541
Bruno Bertol <bbertol2312@outlook.com>
2025-05-25 16:45:17 -0300

    FEAT: dicionário que relacione os números de 1 a 5 aos seus respectivos quadrados.

---
+++ atividade_py/ex002.py
@@ -0,0 +1,11 @@
+dictionary = {
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5
+}
+
+for chave in dictionary:
+    auxiliar = dictionary[chave]
+    print(f'O quadrado de {auxiliar} é {auxiliar**2}')
