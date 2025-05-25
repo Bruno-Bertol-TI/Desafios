66f8fdeaf115b40649349aa7ec4ea6475b31cbf6
Bruno Bertol <bbertol2312@outlook.com>
2025-05-25 16:52:59 -0300

    FEAT: Crie um dicionário e verifique se uma chave específica existe dentro desse dicionário.

--- atividade_py/ex002.py
+++ atividade_py/ex002.py
@@ -1,11 +1,10 @@
-dictionary = {
-    '1': 1,
-    '2': 2,
-    '3': 3,
-    '4': 4,
-    '5': 5
-}
+my_dictionary = {}
 
-for chave in dictionary:
-    auxiliar = dictionary[chave]
-    print(f'O quadrado de {auxiliar} é {auxiliar**2}')
+for chave in range(1, 26):
+    my_dictionary[chave] = 1 + 2 ** 2
+
+print(my_dictionary)
+
+pesquisar_chaves = int(input('Digite a chave que deseja encontrar: '))
+if pesquisar_chaves in my_dictionary:
+    print('Essa chave existe.')
