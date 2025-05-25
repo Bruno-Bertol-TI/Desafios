8a7edca3d72d399bc9245cf84d6414b7542a124e
Bruno Bertol <bbertol2312@outlook.com>
2025-05-25 16:19:42 -0300

    feat: Criando um dicionario com dados.

---
+++ atividade_py/ex001.py
@@ -0,0 +1,10 @@
+dictionary = {
+    'name': 'Bruno',
+    'lastname': 'Bertol',
+    'age': 21,
+    'adress': {
+        'street': 'Rua coronel carlos bardelli',
+        'number': 160,
+        'postal code': '85867-618',      
+    }
+}
